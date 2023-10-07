09ee10d498c35df6a6bc81b28783aa727593b7a0
gilberthappi <gdushimimana6@gmail.com>
2023-10-07 22:16:29 +0200

    correctly files as requested by Reviwer

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,8 +3,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BookIndividual from './BookIndividual';
 
+const staticBooks = [
+  {
+    id: 1,
+    title: 'Sample Book 1',
+    author: 'Author 1',
+  },
+  {
+    id: 2,
+    title: 'Sample Book 2',
+    author: 'Author 2',
+  },
+  {
+    id: 3,
+    title: 'Sample Book 3',
+    author: 'Author 3',
+  },
+];
+
 const BookList = ({ books, onDelete }) => (
   <div>
+    {/* Render static books */}
+    {staticBooks.map((book) => (
+      <BookIndividual key={book.id} book={book} onDelete={onDelete} />
+    ))}
+    {/* Render dynamic books passed as a prop */}
     {books.map((book) => (
       <BookIndividual key={book.id} book={book} onDelete={onDelete} />
     ))}
