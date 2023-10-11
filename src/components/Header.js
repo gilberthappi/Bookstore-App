13befc33f6f70a2a374a98d7b9026c1734d77dd3
gilberthappi <gdushimimana6@gmail.com>
2023-10-12 00:13:52 +0200

    functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <Link to="/">
-      <h1>Bookstore</h1>
+      <h1>OMA Bookstore</h1>
     </Link>
     <nav className="navbar">
       <ul className="navLinks">
         <Link to="/">
           <li className="navLink">Books</li>
         </Link>
-        <Link to="/Catagories">
-          <li className="navLink">Catagories</li>
+        <Link to="/Categories">
+          <li className="navLink">Categories</li>
         </Link>
       </ul>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot from "react-dom/client"
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
 // Use createRoot instead of ReactDOM.render
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
